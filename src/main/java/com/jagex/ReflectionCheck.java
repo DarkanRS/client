e13b07c3d2b860a746bbeaf6e3d064428eb6e247
Trenton Kress <trenton.kress@gmail.com>
2022-08-02 12:08:04 -0700

    Refactor reflection checks more

--- src/main/java/com/jagex/ReflectionCheck.java
+++ src/main/java/com/jagex/ReflectionCheck.java
@@ -6,13 +6,13 @@ import java.lang.reflect.Method;
 public class ReflectionCheck extends Node {
 
     static Class85 aClass85_8039;
-    int anInt8033;
-    Method[] aMethodArray8034;
-    int[] anIntArray8035;
-    int[] anIntArray8036;
-    Field[] aFieldArray8037;
-    int anInt8038;
-    int[] anIntArray8040;
-    byte[][][] aByteArrayArrayArray8041;
+    int size;
+    Method[] methods;
+    int[] types;
+    int[] exceptionCodes;
+    Field[] fields;
+    int id;
+    int[] fieldValues;
+    byte[][][] serializedObjectParams;
 
 }
